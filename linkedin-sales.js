dea3dba8ebd4ef5f4380fcf3334756844f978ae9
Praful Mathur <praful@derivatived.com>
2014-08-08 02:02:18 -0400

    Removed getToday function

--- linkedin-sales.js
+++ linkedin-sales.js
@@ -103,21 +103,6 @@ var moreComments = function() {
   document.getElementById('inline-pagination').click();
 }
 
-var getToday = function() {
-    var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-        var yyyy = today.getFullYear();
-        if(dd<10) {
-          dd='0'+dd
-        } 
-        if(mm<10) {
-        mm='0'+mm
-        } 
-        today = mm+'/'+dd+'/'+yyyy;
-    return today;
-}
-
 /**
 Casper Navigation
 **/
