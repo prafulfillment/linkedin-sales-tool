48aeb76b3e64469734e46c5e74393e85211028a0
Praful Mathur <praful@derivatived.com>
2014-08-12 19:14:46 -0400

    Set Debug to false for production

--- linkedInSales/flaskApp/linkedin-sales.js
+++ linkedInSales/flaskApp/linkedin-sales.js
@@ -2,7 +2,7 @@
  *              IMPORT LIBRARIES                  *
  **************************************************/
 
-var DEBUG = true; 
+var DEBUG = false; 
 
 var casper = require('casper').create({
     logLevel: DEBUG? "debug" : "error",
