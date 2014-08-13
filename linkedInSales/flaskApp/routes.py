3897a25c35fc76609573ca6aabd00178dfedd81c
Praful Mathur <praful@derivatived.com>
2014-08-13 15:18:30 -0400

    Fixed up command dev in routes.py

--- linkedInSales/flaskApp/routes.py
+++ linkedInSales/flaskApp/routes.py
@@ -132,64 +132,56 @@ def addPitch():
 
 @app.route('/sendPitch', methods=['GET', 'POST'])
 def sendPitch():
-  if 'email' not in session:
-    return redirect(url_for('signin'))
+    if 'email' not in session:
+        return redirect(url_for('signin'))
 
-  user = Smarketer.query.filter_by(username = session['email']).first()
+    user = Smarketer.query.filter_by(username = session['email']).first()
 
-  if user is None:
-    return redirect(url_for('signin'))
-  else:
-    form = ConversationStartersForm()
+    if user is None:
+        return redirect(url_for('signin'))
+    else:
+      form = ConversationStartersForm()
 
     if request.method == 'POST':
-      if form.validate() == False:
-        return render_template('sendPitch.html', form=form)
+        if form.validate() == False:
+            return render_template('sendPitch.html', form=form)
 
-      else:
-          ''' 
-          pitch = Pitch.query.filter_by(pitchID = form.pitchID.data).first()
-    	  DiscussionThread = DiscussionThread.query.filter_by(url = form.discussionThreadURL.data).first()
+        else:
+            pitch = Pitch.query.filter_by(pitchID = form.pitchID.data).first()
+            DiscussionThread = DiscussionThread.query.filter_by(url = form.discussionThreadURL.data).first()
 
-	  pitchWarehousePeople = WarehousePeople.query.filter_by(discussionURL = form.discussionThreadURL.data, isPitched = false).limit(form.pitchNumber.data)
-    script_path = os.path.abspath('./flaskApp')
-    commands = ['casperjs {}/linkedin-sales.js'.format(script_path)]
-    commands.append("--user='{}'".format( user.username ))
-    commands.append("--pass='{}'".format( aes_decrypt(user.password) ))
-    commands.append("--first-name='{}'".format( user.firstName ))
-    commands.append("--cookies-file='{}-cookies.txt'".format( user.firstName ))
-    commands.append("--to-user-id=")
-    commands.append("--pitch-subject=")
-    commands.append("--pitch-body=")
-    commands.append("--group-id=")
-    commands.append("--send-pitch={}".format('false'))
-    command = " ".join(commands)
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = proc.communicate()
+            pitchWarehousePeople = WarehousePeople.query.filter_by(discussionURL = form.discussionThreadURL.data, isPitched = false).limit(form.pitchNumber.data)
+            script_path = os.path.abspath('./flaskApp')
+            commands = ['casperjs {}/linkedin-sales.js'.format(script_path)]
+            commands.append("--user='{}'".format( user.username ))
+            commands.append("--pass='{}'".format( aes_decrypt(user.password) ))
+            commands.append("--first-name='{}'".format( user.firstName ))
+            commands.append("--cookies-file='{}-cookies.txt'".format( user.firstName ))
+            commands.append("--send-pitch={}".format('false'))
+            base_command = " ".join(commands)
 
-	  for p in form.pitchNumber.data:
-    	    usernameText = "--user='" + user.username + "'"
-    	    passwordText = "--pass='" + aes_decrypt(user.password) + "'"
-    	    firstNameText = "--first-name='" + user.firstName + "'"
-    	    toUserID = p[pitchWarehousePeople]
-    	    pitchSubject = pitch.subject
-    	    pitchBody = pitch.message
-    	    groupID = discussionThread.groupID
-     	    sendPitch = not DEBUG
-    	    proc = subprocess.Popen("casperjs linkedin-sales.js --user={0} --pass={1} --first-name={3} --to-user-id={3} --pitch-subject={4} --pitch-body={5} --group-id={6} --send-pitch={7}".format(usernameText, passwordText, firstNameText, toUserID, pitchSubject, pitchBody, groupID, sendPitch), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            for p in form.pitchNumber.data:
+                pitch_commands = []
+                pitch_commands.append("--to-user-id='{}'".format(p[pitchWarehousePeople]))
+                pitch_commands.append("--pitch-subject=")pitch.subject
+                pitch_commands.append("--pitch-body=")pitch.message
+                pitch_commands.append("--group-id=")discussionThread.groupID
+                pitch_command = "{0} {1}".format(base_command, " ".join(pitch_commands))
+                proc = subprocess.Popen(pitch_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output = proc.communicate()
+                status = json.loads(output[0])
 
-	    #TODO: open subprocess, return true or false for this warehousePerson
- 
-	    #saves new conversationStarters in database
-            #newConversationStarter = ConversationStarters(form.url.data, form.groupID.data, form.title.data)
-    	    #db.session.add(newConversationStarter)
-       	    #db.session.commit()
-            '''
-	  form.pitchNumber.data = ""
-          return render_template('sendPitch.html', form=form, success=True) #TODO: insert array to tell which ones failed
+                #TODO: open subprocess, return true or false for this warehousePerson
+
+                #saves new conversationStarters in database
+                #newConversationStarter = ConversationStarters(form.url.data, form.groupID.data, form.title.data)
+                #db.session.add(newConversationStarter)
+                #db.session.commit()
+                form.pitchNumber.data = ""
+            return render_template('sendPitch.html', form=form, success=True) #TODO: insert array to tell which ones failed
 
     elif request.method == 'GET':
-      return render_template('sendPitch.html', form=form)
+        return render_template('sendPitch.html', form=form)
 
 @app.route('/addDiscussionThread', methods=['GET', 'POST'])
 def addDiscussionThread():
