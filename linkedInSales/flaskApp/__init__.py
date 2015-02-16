7913d5cdc90c4fa14a139788ee59cf0a67b6f1d6
jamespsteinberg <jamespsteinberg@gmail.com>
2015-02-15 20:34:53 -0800

    removed secrets

--- linkedInSales/flaskApp/__init__.py
+++ linkedInSales/flaskApp/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.debug = True
 
 # set a 'SECRET_KEY' to enable the Flask session cookies
-app.config['SECRET_KEY'] = 'supersecretdevkey'
+app.config['SECRET_KEY'] = 'devkeythatencrypesthings'
 
 toolbar = DebugToolbarExtension(app)
 
@@ -18,13 +18,13 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'james@derivatived.com'
-app.config["MAIL_PASSWORD"] = aes_decrypt('64cb6dfee1a02d585a4eb9125458f3f2')
+app.config["MAIL_USERNAME"] = 'YOUREMAIL@YOURDOMAIN.com'
+app.config["MAIL_PASSWORD"] = aes_decrypt('YOUR ENCRYPTED PASSWORD HERE')
 
 from routes import mail
 mail.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:02cool4coldmessaging@localhost/linkedInSales'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:PASSWORD@localhost/DATABASE'
 
 from models import db
 db.init_app(app)
